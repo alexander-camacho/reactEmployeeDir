0f703e93b69dfd04caa50a2b43d03719ea32eb5f
Alex Camacho <alsbrain@optonline.net>
2021-03-05 12:14:14 -0800

    cleaned up starter file, add component folder with test components.

---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Table from "./Table"
+
+function Home() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Table />
+            </header>
+        </div>
+    )
+}
+
+export default Home
---
+++ src/components/Table.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+function Table() {
+    return (
+        <table>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+            </tr>
+            <tr>
+                <td>Alex</td>
+                <td>Camacho</td>
+            </tr>
+            <tr>
+                <td>John</td>
+                <td>Smith</td>
+            </tr>
+        </table>
+    )
+}
+
+export default Table
